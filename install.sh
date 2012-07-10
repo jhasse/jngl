c293001056a6011d1bfcc676e79d7ee939385b82
Jan Niklas Hasse <jhasse@gmail.com>
2012-07-11 00:52:01 +0200

    print message when called without prefix

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 prefix=$1
+if [ "$prefix" == "" ]; then
+	echo "Usage: ./install.sh <prefix>"
+	exit 1
+fi
 mkdir -p $prefix/include/
 mkdir -p $prefix/lib/
 mkdir -p $prefix/lib/pkgconfig/
-cp jngl.hpp $prefix/include/
+cp src/jngl.hpp $prefix/include/
 cp libjngl.a $prefix/lib/
 cp jngl.pc $prefix/lib/pkgconfig/
