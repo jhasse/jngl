003a2b15dcdc603b6c8ca255f62f5030c8b70e68
Jan Niklas Hasse <jhasse@gmail.com>
2012-09-18 17:41:43 +0200

    implement setKeyboardVisible for Windows and Linux

---
+++ src/win32/input.cpp
@@ -0,0 +1,11 @@
+/*
+Copyright 2012 Jan Niklas Hasse <jhasse@gmail.com>
+For conditions of distribution and use, see copyright notice in LICENSE.txt
+*/
+
+#include "../jngl/input.hpp"
+
+namespace jngl {
+	void setKeyboardVisible(bool) {
+	}
+}
