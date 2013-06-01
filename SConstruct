074fa638034d8c564afa4951d59e690b85e25212
Jan Niklas Hasse <jhasse@gmail.com>
2013-06-01 16:37:16 +0200

    Use C++11's std::chrono functions instead of glib-2.0

--- SConstruct
+++ SConstruct
@@ -92,7 +92,7 @@ if env['PLATFORM'] == 'posix': # Linux
 	if env['python']:
 		env.Append(CCFLAGS = '-DNOJPEG')
 	source_files += env.Object(Glob('src/linux/*.cpp'), CPPFLAGS='-std=c++0x')
-	env.ParseConfig('pkg-config --cflags --libs fontconfig glib-2.0')
+	env.ParseConfig('pkg-config --cflags --libs fontconfig')
 	env.ParseConfig('pkg-config --cflags --libs freetype2')
 	env.Append(CCFLAGS="-fPIC -DNO_WEAK_LINKING_OPENAL")
 	lib = env.Library(target="jngl", source=source_files)
--- src/linux/time.cpp
+++ src/linux/time.cpp
@@ -1,41 +1,19 @@
 /*
-Copyright 2007 Jan Niklas Hasse <jhasse@gmail.com>
-
-This file is part of JNGL.
-
-JNGL is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-JNGL is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with jngl.  If not, see <http://www.gnu.org/licenses/>.
+Copyright 2007-2013 Jan Niklas Hasse <jhasse@gmail.com>
+For conditions of distribution and use, see copyright notice in LICENSE.txt
 */
 
-#include <glib.h>
-#include <unistd.h>
+#include <chrono>
+#include <thread>
 
-namespace jngl
-{
-	struct Timer
-	{
-		Timer() : gtimer_(g_timer_new()) {}
-		~Timer() { g_timer_destroy(gtimer_); }
-		GTimer* gtimer_;
-	};
-	double getTime()
-	{
-		static Timer timer;
-		return g_timer_elapsed(timer.gtimer_, NULL);
+namespace jngl {
+	using namespace std::chrono;
+
+	double getTime() {
+		static auto start = high_resolution_clock::now();
+		return duration_cast<duration<double>>(high_resolution_clock::now() - start).count();
 	}
-	void sleep(int milliseconds)
-	{
-		usleep(milliseconds * 1000);
+	void sleep(int ms) {
+		std::this_thread::sleep_for(milliseconds(ms));
 	}
 }
-
