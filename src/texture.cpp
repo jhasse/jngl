d00d52e4c4728766153ed31f4613e8c9fcf59398
Jan Niklas Hasse <jhasse@gmail.com>
2015-01-26 23:30:58 +0100

    Use integer version of glTexParameter

--- src/texture.cpp
+++ src/texture.cpp
@@ -23,8 +23,8 @@ namespace jngl {
 		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // preventing wrapping artifacts
-		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // preventing wrapping artifacts
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		GLfloat vertexes[] = {
 			0, 0, 0, 1, 1, 1, 1, 0, // texture coordinates
 			0, 0, 0, GLfloat(height), GLfloat(width), GLfloat(height), GLfloat(width), 0
