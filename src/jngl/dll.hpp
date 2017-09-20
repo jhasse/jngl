8ab0c11037e209e8b8d6df4004ddee74039187c0
Jan Niklas Hasse <jhasse@bixense.com>
2017-09-20 14:57:39 +0200

    Don't redefine JNGLDLL_API

--- src/jngl/dll.hpp
+++ src/jngl/dll.hpp
@@ -1,16 +1,18 @@
 /*
-Copyright 2014 Jan Niklas Hasse <jhasse@gmail.com>
+Copyright 2014-2017 Jan Niklas Hasse <jhasse@gmail.com>
 For conditions of distribution and use, see copyright notice in LICENSE.txt
 */
 
 #pragma once
 
-#ifdef _MSC_VER
-#ifdef JNGLDLL_EXPORTS
-#define JNGLDLL_API __declspec(dllexport)
-#else
-#define JNGLDLL_API __declspec(dllimport)
-#endif
-#else
-#define JNGLDLL_API
+#ifndef JNGLDLL_API
+	#ifdef _MSC_VER
+		#ifdef JNGLDLL_EXPORTS
+			#define JNGLDLL_API __declspec(dllexport)
+		#else
+			#define JNGLDLL_API __declspec(dllimport)
+		#endif
+	#else
+		#define JNGLDLL_API
+	#endif
 #endif
