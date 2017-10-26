be0de8a59221c8df88866b0803c6de4c819da2b6
Jan Niklas Hasse <jhasse@bixense.com>
2017-10-26 19:59:18 +0200

    Support for Xbox controller (SDL2)

--- src/sdl/input.cpp
+++ src/sdl/input.cpp
@@ -69,24 +69,28 @@ namespace jngl {
 		if (it == joysticks.end()) {
 			return false;
 		}
+		const bool xbox = SDL_JoystickNumButtons(it->second) == 15;
+		if (xbox && (button == controller::LeftTrigger || button == controller::RightTrigger)) {
+			return getControllerState(number, button) > 0;
+		}
 		int buttonIndex;
 		switch (button) {
 			case controller::A: buttonIndex = 0; break;
 			case controller::B: buttonIndex = 1; break;
-			case controller::X: buttonIndex = 3; break;
-			case controller::Y: buttonIndex = 2; break;
+			case controller::X: buttonIndex = xbox ? 2 : 3; break;
+			case controller::Y: buttonIndex = xbox ? 3 : 2; break;
 			case controller::LeftButton: buttonIndex = 4; break;
 			case controller::RightButton: buttonIndex = 5; break;
 			case controller::LeftTrigger: buttonIndex = 6; break;
 			case controller::RightTrigger: buttonIndex = 7; break;
-			case controller::Start: buttonIndex = 9; break;
-			case controller::Back: buttonIndex = 8; break;
+			case controller::Start: buttonIndex = xbox ? 7 : 9; break;
+			case controller::Back: buttonIndex = xbox ? 6 : 8; break;
 			case controller::DpadUp: return SDL_JoystickGetHat(it->second, 0) & SDL_HAT_UP;
 			case controller::DpadDown: return SDL_JoystickGetHat(it->second, 0) & SDL_HAT_DOWN;
 			case controller::DpadLeft: return SDL_JoystickGetHat(it->second, 0) & SDL_HAT_LEFT;
 			case controller::DpadRight: return SDL_JoystickGetHat(it->second, 0) & SDL_HAT_RIGHT;
-			case controller::LeftStick: buttonIndex = 11; break;
-			case controller::RightStick: buttonIndex = 12; break;
+			case controller::LeftStick: buttonIndex = xbox ? 9 : 11; break;
+			case controller::RightStick: buttonIndex = xbox ? 10 : 12; break;
 			default: return false;
 		}
 		return SDL_JoystickGetButton(it->second, buttonIndex);
