1d77a5397fad566f6a5a8d80c786a331438c32d7
Jan Niklas Hasse <jhasse@bixense.com>
2017-11-04 18:48:09 +0100

    Implement Window::SetIcon for SDL2

--- src/sdl/window.cpp
+++ src/sdl/window.cpp
@@ -5,6 +5,11 @@
 #include "../main.hpp"
 #include "sdl.hpp"
 #include "windowimpl.hpp"
+#include "../jngl/debug.hpp"
+
+#ifndef NOPNG
+#include <png.h>
+#endif
 
 namespace jngl {
 	Window::Window(const std::string& title, const int width, const int height, const bool fullscreen)
@@ -261,8 +266,69 @@ namespace jngl {
 		}
 	}
 
-	void Window::SetIcon(const std::string&) {
-		return; // TODO: Not implemented yet
+	void Window::SetIcon(const std::string& filename) {
+#ifndef NOPNG
+		FILE* fp = fopen(filename.c_str(), "rb");
+		if (!fp)
+			throw std::runtime_error(std::string("File not found: ") + filename);
+		Finally _([&fp]() {
+			fclose(fp);
+		});
+		png_byte buf[PNG_BYTES_TO_CHECK];
+		assert(PNG_BYTES_TO_CHECK >= sizeof(unsigned short));
+
+		// Read in some of the signature bytes
+		if (fread(buf, 1, PNG_BYTES_TO_CHECK, fp) != PNG_BYTES_TO_CHECK)
+			throw std::runtime_error(std::string("Error reading signature bytes."));
+
+		assert(png_sig_cmp(buf, (png_size_t)0, PNG_BYTES_TO_CHECK) == 0);
+		png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+		if (!png_ptr) {
+			throw std::runtime_error("libpng error while reading");
+		}
+
+		png_infop info_ptr = png_create_info_struct(png_ptr);
+		if (!info_ptr) {
+			throw std::runtime_error("libpng error while reading");
+		}
+
+		if (setjmp(png_jmpbuf(png_ptr))) {
+			// Free all of the memory associated with the png_ptr and info_ptr
+			png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+			throw std::runtime_error("Error reading file.");
+		}
+		png_init_io(png_ptr, fp);
+		png_set_sig_bytes(png_ptr, PNG_BYTES_TO_CHECK);
+		int colorType = png_get_color_type(png_ptr, info_ptr);
+		if (colorType == PNG_COLOR_TYPE_GRAY || colorType == PNG_COLOR_TYPE_GRAY_ALPHA) {
+			png_set_gray_to_rgb(png_ptr);
+		}
+		png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_STRIP_16, NULL);
+
+		const int x = png_get_image_width(png_ptr, info_ptr);
+		const int y = png_get_image_height(png_ptr, info_ptr);
+		const int channels = png_get_channels(png_ptr, info_ptr);
+
+		const auto row_pointers = png_get_rows(png_ptr, info_ptr);
+		std::vector<char> imageData(x * y * channels);
+		for (int i = 0; i < y; ++i) {
+			memcpy(&imageData[i*x*channels], row_pointers[i], x * channels);
+		}
+
+		SDL_Surface* const surface =
+		    SDL_CreateRGBSurfaceFrom(&imageData[0], x, y, channels * 8, channels * x, 0x000000ff,
+		                             0x0000ff00, 0x00ff0000, 0xff000000);
+
+		if (!surface) {
+			jngl::debugLn(SDL_GetError());
+			return;
+		}
+
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+
+		SDL_SetWindowIcon(impl->sdlWindow, surface);
+		SDL_FreeSurface(surface);
+#endif
 	}
 
 	int getDesktopWidth() {
