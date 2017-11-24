67d1fac490898ac70645343d8252dee4d11ae19b
Jan Niklas Hasse <jhasse@bixense.com>
2017-11-24 17:11:13 +0100

    Ignore DS4 Motion Sensor

--- src/sdl/input.cpp
+++ src/sdl/input.cpp
@@ -12,15 +12,17 @@ namespace jngl {
 	void setKeyboardVisible(bool) {
 	}
 
-	std::map<int, SDL_Joystick*> joysticks;
-
-
 	std::vector<std::shared_ptr<Controller>> getConnectedControllers() {
 		static std::vector<std::shared_ptr<SdlController>> controllers;
 		int numJoysticks = SDL_NumJoysticks();
 		std::vector<std::shared_ptr<Controller>> rtn;
 		for (int i = 0; i < numJoysticks; ++i) {
 			SDL_Joystick* handle = SDL_JoystickOpen(i);
+			if (SDL_JoystickNumButtons(handle) == 0) {
+				// This could be the Motion Sensors of the DS4. Ignore it:
+				SDL_JoystickClose(handle);
+				continue;
+			}
 			bool found = false;
 			for (const auto& controller : controllers) {
 				if (controller->is(handle)) {
@@ -32,32 +34,18 @@ namespace jngl {
 			if (!found) {
 				controllers.emplace_back(std::make_shared<SdlController>(handle));
 				rtn.emplace_back(controllers.back());
+				jngl::debug("Name: ");
+				jngl::debugLn(SDL_JoystickNameForIndex(i));
+				jngl::debug("Number of Axes: ");
+				jngl::debugLn(SDL_JoystickNumAxes(handle));
+				jngl::debug("Number of Buttons: ");
+				jngl::debugLn(SDL_JoystickNumButtons(handle));
+				jngl::debug("Number of Balls: ");
+				jngl::debugLn(SDL_JoystickNumBalls(handle));
+				jngl::debug("Number of Hats: ");
+				jngl::debugLn(SDL_JoystickNumHats(handle));
 			}
 		}
 		return rtn;
-	}
-
-	bool isControllerConnected(const int number) {
-		const auto it = joysticks.find(number);
-		if (it == joysticks.end()) {
-			if (number >= SDL_NumJoysticks()) {
-				return false;
-			}
-			joysticks[number] = SDL_JoystickOpen(number);
-
-			jngl::debug("Name: ");
-			jngl::debugLn(SDL_JoystickNameForIndex(number));
-			jngl::debug("Number of Axes: ");
-			jngl::debugLn(SDL_JoystickNumAxes(joysticks[number]));
-			jngl::debug("Number of Buttons: ");
-			jngl::debugLn(SDL_JoystickNumButtons(joysticks[number]));
-			jngl::debug("Number of Balls: ");
-			jngl::debugLn(SDL_JoystickNumBalls(joysticks[number]));
-			jngl::debug("Number of Hats: ");
-			jngl::debugLn(SDL_JoystickNumHats(joysticks[number]));
-
-			return true;
-		}
-		return SDL_JoystickGetAttached(it->second);
 	}
 }
