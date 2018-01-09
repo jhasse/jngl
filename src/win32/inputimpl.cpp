713bc7cd7a8114e869d3df676b0e59cb2f5c4667
Jan Niklas Hasse <jhasse@bixense.com>
2018-01-09 11:51:34 +0100

    Include xinput.h with lower-case X for cross-compilation

--- src/win32/inputimpl.cpp
+++ src/win32/inputimpl.cpp
@@ -6,26 +6,28 @@
 #include "XinputController.hpp"
 
 #include <windows.h>
-#include <Xinput.h>
+#include <xinput.h>
 
 namespace jngl {
-	void setKeyboardVisible(bool) {
-	}
 
-	std::vector<std::shared_ptr<Controller>> getConnectedControllers() {
-		static std::shared_ptr<XinputController> controllers[XUSER_MAX_COUNT];
-		std::vector<std::shared_ptr<Controller>> tmp;
-		for (int i = 0; i < XUSER_MAX_COUNT; ++i) {
-			XINPUT_STATE state;
-			ZeroMemory(&state, sizeof(XINPUT_STATE));
-    		DWORD dwResult = XInputGetState(i, &state);
-			if (dwResult == ERROR_SUCCESS) {
-				if (!tmp[i]) {
-					tmp[i] = std::make_shared<XinputController>(i);
-				}
-				tmp.push_back(tmp[i]);
-			}
-		}
-		return tmp;
-	}
+void setKeyboardVisible(bool) {
 }
+
+std::vector<std::shared_ptr<Controller>> getConnectedControllers() {
+	static std::shared_ptr<XinputController> controllers[XUSER_MAX_COUNT];
+	std::vector<std::shared_ptr<Controller>> tmp;
+	for (int i = 0; i < XUSER_MAX_COUNT; ++i) {
+		XINPUT_STATE state;
+		ZeroMemory(&state, sizeof(XINPUT_STATE));
+		DWORD dwResult = XInputGetState(i, &state);
+		if (dwResult == ERROR_SUCCESS) {
+			if (!tmp[i]) {
+				tmp[i] = std::make_shared<XinputController>(i);
+			}
+			tmp.push_back(tmp[i]);
+		}
+	}
+	return tmp;
+}
+
+} // namespace jngl
