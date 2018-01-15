490879083342287b3fd0454dc040efa878495390
Jan Niklas Hasse <jhasse@bixense.com>
2018-01-15 16:45:42 +0100

    Format work.cpp

--- src/jngl/work.cpp
+++ src/jngl/work.cpp
@@ -1,18 +1,18 @@
-/*
-Copyright 2012 Jan Niklas Hasse <jhasse@gmail.com>
-For conditions of distribution and use, see copyright notice in LICENSE.txt
-*/
+// Copyright 2012-2018 Jan Niklas Hasse <jhasse@gmail.com>
+// For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "all.hpp"
 
 namespace jngl {
-	void Work::onQuitEvent() {
-		jngl::quit();
-	}
 
-	void Work::onLoad() {
-	}
-
-	Work::~Work() {
-	}
+void Work::onQuitEvent() {
+	jngl::quit();
 }
+
+void Work::onLoad() {
+}
+
+Work::~Work() {
+}
+
+} // namespace jngl
