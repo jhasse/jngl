9825045ad17080b040b86cf8f00f736c0d136598
Jan Niklas Hasse <jhasse@bixense.com>
2018-01-29 09:50:35 +0100

    Fix clang-tidy warnings in src/linux/window.cpp

--- src/linux/window.cpp
+++ src/linux/window.cpp
@@ -1,40 +1,38 @@
-/*
-Copyright 2007-2015 Jan Niklas Hasse <jhasse@gmail.com>
-For conditions of distribution and use, see copyright notice in LICENSE.txt
-*/
+// Copyright 2007-2018 Jan Niklas Hasse <jhasse@bixense.com>
+// For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "../window.hpp"
 
 #include <fontconfig/fontconfig.h>
 
 namespace jngl {
-	std::string Window::GetFontFileByName(const std::string& fontname) {
-		const double size = 12;
-		FcFontSet* fontSet = FcFontSetCreate();
 
-		FcPattern* pattern = FcPatternBuild(NULL, FC_FAMILY, FcTypeString, fontname.c_str(),
-		                                    FC_SIZE, FcTypeDouble, size, NULL);
-		FcConfigSubstitute(NULL,pattern, FcMatchPattern);
-		FcDefaultSubstitute(pattern);
+std::string Window::GetFontFileByName(const std::string& fontname) {
+	const double size = 12;
+	FcFontSet* fontSet = FcFontSetCreate();
 
-		FcResult result;
-		FcPattern* match = FcFontMatch(NULL, pattern, &result);
-		FcPatternDestroy(pattern);
+	FcPattern* pattern = FcPatternBuild(nullptr, FC_FAMILY, FcTypeString, fontname.c_str(), FC_SIZE,
+	                                    FcTypeDouble, size, nullptr);
+	FcConfigSubstitute(nullptr, pattern, FcMatchPattern);
+	FcDefaultSubstitute(pattern);
 
-		if(!match)
-		{
-			FcFontSetDestroy(fontSet);
-			throw std::runtime_error(std::string("Couldn't load " + fontname));
-		}
-		FcFontSetAdd(fontSet, match);
+	FcResult result;
+	FcPattern* match = FcFontMatch(nullptr, pattern, &result);
+	FcPatternDestroy(pattern);
 
-		if(fontSet->nfont == 0)
-		{
-			throw std::runtime_error(std::string("Couldn't load " + fontname));
-		}
-
-		FcChar8* filename = NULL;
-		FcPatternGetString(fontSet->fonts[0], FC_FILE, 0, &filename);
-		return (const char*)filename;
+	if (!match) {
+		FcFontSetDestroy(fontSet);
+		throw std::runtime_error(std::string("Couldn't load " + fontname));
 	}
+	FcFontSetAdd(fontSet, match);
+
+	if (fontSet->nfont == 0) {
+		throw std::runtime_error(std::string("Couldn't load " + fontname));
+	}
+
+	FcChar8* filename = nullptr;
+	FcPatternGetString(fontSet->fonts[0], FC_FILE, 0, &filename);
+	return reinterpret_cast<const char*>(filename);
 }
+
+} // namespace jngl
