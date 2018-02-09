f6fc6a299246cda6003c35b4875ef66482d41bf1
Jan Niklas Hasse <jhasse@bixense.com>
2018-02-09 16:16:56 +0100

    Use steady_clock instead of high_resolution_clock to be resistant against time changes

--- src/jngl/time.cpp
+++ src/jngl/time.cpp
@@ -1,7 +1,5 @@
-/*
-Copyright 2007-2013 Jan Niklas Hasse <jhasse@gmail.com>
-For conditions of distribution and use, see copyright notice in LICENSE.txt
-*/
+// Copyright 2007-2018 Jan Niklas Hasse <jhasse@bixense.com>
+// For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include <chrono>
 #include <thread>
@@ -10,8 +8,8 @@ namespace jngl {
 	using namespace std::chrono;
 
 	double getTime() {
-		static auto start = high_resolution_clock::now();
-		return duration_cast<duration<double>>(high_resolution_clock::now() - start).count();
+		static auto start = steady_clock::now();
+		return duration_cast<duration<double>>(steady_clock::now() - start).count();
 	}
 	void sleep(int ms) {
 		std::this_thread::sleep_for(milliseconds(ms));
