f57353108fbcd9aff617ac555e39567642d744e4
Jan Niklas Hasse <jhasse@bixense.com>
2018-02-18 00:01:18 +0100

    Format texture.hpp

--- src/texture.hpp
+++ src/texture.hpp
@@ -1,4 +1,4 @@
-// Copyright 2010-2017 Jan Niklas Hasse <jhasse@gmail.com>
+// Copyright 2010-2018 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #pragma once
@@ -11,29 +11,32 @@
 #include <memory>
 
 namespace jngl {
-	class Texture {
-	public:
-		Texture(int imgWidth, int imgHeight,
-		        GLubyte** rowPointers, // data as row pointers ...
-		        GLenum format = GL_RGBA, GLubyte* data = nullptr /* ... or as one pointer */ );
-		Texture(const Texture&) = delete;
-		Texture& operator=(const Texture&) = delete;
-		~Texture();
-		void Bind() const;
-		void draw() const;
-		void drawClipped(float xstart, float xend, float ystart, float yend) const;
-		GLuint getID() const;
-		int getWidth() const;
-		int getHeight() const;
-	private:
-		GLuint texture_;
-		GLuint vertexBuffer_;
-		GLuint vao;
-		std::vector<opengl::CoordType> texCoords_;
-		std::vector<GLfloat> vertexes_;
-		int width;
-		int height;
-	};
 
-	extern std::unordered_map<std::string, std::shared_ptr<Texture>> textures;
-}
+class Texture {
+public:
+	Texture(int imgWidth, int imgHeight,
+	        GLubyte** rowPointers, // data as row pointers ...
+	        GLenum format = GL_RGBA, GLubyte* data = nullptr /* ... or as one pointer */);
+	Texture(const Texture&) = delete;
+	Texture& operator=(const Texture&) = delete;
+	~Texture();
+	void Bind() const;
+	void draw() const;
+	void drawClipped(float xstart, float xend, float ystart, float yend) const;
+	GLuint getID() const;
+	int getWidth() const;
+	int getHeight() const;
+
+private:
+	GLuint texture_;
+	GLuint vertexBuffer_;
+	GLuint vao;
+	std::vector<opengl::CoordType> texCoords_;
+	std::vector<GLfloat> vertexes_;
+	int width;
+	int height;
+};
+
+extern std::unordered_map<std::string, std::shared_ptr<Texture>> textures;
+
+} // namespace jngl
