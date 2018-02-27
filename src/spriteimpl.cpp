93f7ebd8cdd3997250a633c54e074d515780ba1e
Jan Niklas Hasse <jhasse@bixense.com>
2018-02-27 14:13:52 +0100

    Don't include png.h in spriteimpl if NOPNG is defined

--- src/spriteimpl.cpp
+++ src/spriteimpl.cpp
@@ -1,7 +1,9 @@
 // Copyright 2007-2018 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
+#ifndef NOPNG
 #include <png.h> // We need to include it first, I don't know why
+#endif
 
 #include "spriteimpl.hpp"
 #include "window.hpp"
