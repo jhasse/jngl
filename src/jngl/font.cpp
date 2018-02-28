d44c24f03576098bdd7b22c3360780cbfeec8807
Jan Niklas Hasse <jhasse@bixense.com>
2018-02-28 11:51:46 +0100

    Remove redundant call to 'c_str'

--- src/jngl/font.cpp
+++ src/jngl/font.cpp
@@ -9,42 +9,43 @@
 #include <stack>
 
 namespace jngl {
-	unsigned char fontColorRed = 0, fontColorGreen = 0, fontColorBlue = 0, fontColorAlpha = 255;
 
-	void setFontColor(const jngl::Color color) {
-		setFontColor(color.getRed(), color.getGreen(), color.getBlue());
-	}
+unsigned char fontColorRed = 0, fontColorGreen = 0, fontColorBlue = 0, fontColorAlpha = 255;
 
-	void setFontColor(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha) {
-		fontColorRed = red;
-		fontColorGreen = green;
-		fontColorBlue = blue;
-		fontColorAlpha = alpha;
-	}
-
-	std::stack<Color> rgbs;
-
-	void pushFontColor(unsigned char red, unsigned char green, unsigned char blue) {
-		rgbs.push(Color(fontColorRed, fontColorGreen, fontColorBlue));
-		setFontColor(red, green, blue);
-	}
-
-	void popFontColor() {
-		fontColorRed = rgbs.top().getRed();
-		fontColorGreen = rgbs.top().getGreen();
-		fontColorBlue = rgbs.top().getBlue();
-		rgbs.pop();
-	}
-
-	Font::Font(const std::string& filename, unsigned int size)
-	: impl(new FontImpl(filename.c_str(), size)) {
-	}
-
-	void Font::print(const std::string& text, int x, int y) {
-		impl->print(x, y, text);
-	}
-
-	std::shared_ptr<FontImpl> Font::getImpl() {
-		return impl;
-	}
+void setFontColor(const jngl::Color color) {
+	setFontColor(color.getRed(), color.getGreen(), color.getBlue());
 }
+
+void setFontColor(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha) {
+	fontColorRed = red;
+	fontColorGreen = green;
+	fontColorBlue = blue;
+	fontColorAlpha = alpha;
+}
+
+std::stack<Color> rgbs;
+
+void pushFontColor(unsigned char red, unsigned char green, unsigned char blue) {
+	rgbs.push(Color(fontColorRed, fontColorGreen, fontColorBlue));
+	setFontColor(red, green, blue);
+}
+
+void popFontColor() {
+	fontColorRed = rgbs.top().getRed();
+	fontColorGreen = rgbs.top().getGreen();
+	fontColorBlue = rgbs.top().getBlue();
+	rgbs.pop();
+}
+
+Font::Font(const std::string& filename, unsigned int size) : impl(new FontImpl(filename, size)) {
+}
+
+void Font::print(const std::string& text, int x, int y) {
+	impl->print(x, y, text);
+}
+
+std::shared_ptr<FontImpl> Font::getImpl() {
+	return impl;
+}
+
+} // namespace jngl
