ff125a3c7b073f71f7f230aed2cd85f69761f8ad
Jan Niklas Hasse <jhasse@bixense.com>
2018-06-18 13:43:42 +0200

    Use getMousePos instead of getMouseX/Y for Android Test

--- android/test/app/src/main/cpp/main.cpp
+++ android/test/app/src/main/cpp/main.cpp
@@ -1,18 +1,22 @@
 #include <jngl.hpp>
 
 JNGL_MAIN_BEGIN {
-    try {
-        jngl::showWindow("Android Test", 1920, 1080);
-        float x = 0;
-        while (jngl::running()) {
-            jngl::updateInput();
-            jngl::setBackgroundColor(0, 255, 0);
-            jngl::setColor(0, 0, 255);
-            jngl::drawRect(-jngl::getWindowWidth() / 2, -jngl::getWindowHeight() / 2, jngl::getWindowWidth() - jngl::getMouseX(), jngl::getMouseY());
-            x += 0.4;
-            jngl::swapBuffers();
-        }
-    } catch(std::exception& e) {
-        jngl::errorMessage(e.what());
-    }
-} JNGL_MAIN_END
+	try {
+		jngl::showWindow("Android Test", 2560, 1440);
+		float x = 0;
+		while (jngl::running()) {
+			jngl::updateInput();
+			jngl::setBackgroundColor(0, 255, 0);
+			jngl::setColor(0, 0, 255);
+			const auto mouse = jngl::getMousePos();
+			jngl::drawRect(-jngl::getScreenWidth() / 2, -jngl::getScreenHeight() / 2,
+			               jngl::getScreenWidth() / 2 - mouse.x,
+			               jngl::getScreenHeight() / 2 + mouse.y);
+			x += 0.4;
+			jngl::swapBuffers();
+		}
+	} catch (std::exception& e) {
+		jngl::errorMessage(e.what());
+	}
+}
+JNGL_MAIN_END
