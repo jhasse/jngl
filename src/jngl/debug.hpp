1c8a5ccab2ec149291abf509edf6312e84a6f345
Jan Niklas Hasse <jhasse@bixense.com>
2018-07-01 17:45:36 +0200

    Fix -Wunused-parameter in debug.hpp

--- src/jngl/debug.hpp
+++ src/jngl/debug.hpp
@@ -1,7 +1,5 @@
-/*
-Copyright 2012-2015 Jan Niklas Hasse <jhasse@gmail.com>
-For conditions of distribution and use, see copyright notice in LICENSE.txt
-*/
+// Copyright 2012-2018 Jan Niklas Hasse <jhasse@gmail.com>
+// For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #pragma once
 
@@ -12,21 +10,25 @@ For conditions of distribution and use, see copyright notice in LICENSE.txt
 #endif
 
 namespace jngl {
-	template<class T>
-	void debug(const T& t) {
-	#ifndef NDEBUG
-		std::stringstream s;
-		s << t;
-		printMessage(s.str());
-	#endif
-	}
-
-	template<class T>
-	void debugLn(const T& t) {
-	#ifndef NDEBUG
-		std::stringstream s;
-		s << t << std::endl;
-		printMessage(s.str());
-	#endif
-	}
+template <class T>
+#ifdef NDEBUG
+void debug(const T&) {
+#else
+void debug(const T& t) {
+	std::stringstream s;
+	s << t;
+	printMessage(s.str());
+#endif
 }
+
+template <class T>
+#ifndef NDEBUG
+void debugLn(const T&) {
+#else
+void debugLn(const T& t) {
+	std::stringstream s;
+	s << t << std::endl;
+	printMessage(s.str());
+#endif
+}
+} // namespace jngl
