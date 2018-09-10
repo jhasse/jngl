ac4c741cc491024511f452e630a5c9577b05b5b9
Jan Niklas Hasse <jhasse@bixense.com>
2018-09-10 18:43:01 +0200

    Windows and macOS implementation of getBinaryPath

---
+++ src/win32/other.cpp
@@ -0,0 +1,13 @@
+// Copyright 2018 Jan Niklas Hasse <jhasse@bixense.com>
+// For conditions of distribution and use, see copyright notice in LICENSE.txt
+
+#include "../jngl/other.hpp"
+
+#include <windows.h>
+
+std::string jngl::getBinaryPath() {
+	char filename[MAX_PATH];
+	GetModuleFileName(nullptr, filename, MAX_PATH);
+	std::string tmp(filename);
+	*binpath = tmp.substr(0, tmp.find_last_of("\\/") + 1);
+}
