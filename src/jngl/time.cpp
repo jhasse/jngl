17c42d541a3885d005a3903ba5f32feddb62ed05
Jan Niklas Hasse <jhasse@bixense.com>
2018-09-20 16:03:04 +0200

    Export functions in time.cpp and window.cpp for MSVC

--- src/jngl/time.cpp
+++ src/jngl/time.cpp
@@ -1,19 +1,21 @@
 // Copyright 2007-2018 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
+#include "dll.hpp"
+
 #include <chrono>
 #include <thread>
 
 namespace jngl {
 
-double getTime() {
+double JNGLDLL_API getTime() {
 	static auto start = std::chrono::steady_clock::now();
 	return std::chrono::duration_cast<std::chrono::duration<double>>(
 	           std::chrono::steady_clock::now() - start)
 	    .count();
 }
 
-void sleep(const int milliseconds) {
+void JNGLDLL_API sleep(const int milliseconds) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
 
