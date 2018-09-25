f4479c2d0b306aa73001dc0a0c2918c9f64d422c
Jan Niklas Hasse <jhasse@bixense.com>
2018-09-25 19:27:57 +0200

    iOS: Port Object-C++ code to OpenGL ES 3.0

--- src/ios/JNGLView.h
+++ src/ios/JNGLView.h
@@ -1,8 +1,8 @@
 #import <UIKit/UIKit.h>
 #import <OpenGLES/EAGL.h>
 #import <QuartzCore/QuartzCore.h>
-#import <OpenGLES/ES1/gl.h>
-#import <OpenGLES/ES1/glext.h>
+#import <OpenGLES/ES3/gl.h>
+#import <OpenGLES/ES3/glext.h>
 
 namespace jngl {
 	class WindowImpl;
