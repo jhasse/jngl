e12fcf79a6b036ccd9cff339ba915e4c1148287e
Jan Niklas Hasse <jhasse@bixense.com>
2018-09-26 11:49:34 +0200

    Format src/spriteimpl.hpp

--- src/spriteimpl.hpp
+++ src/spriteimpl.hpp
@@ -1,4 +1,4 @@
-// Copyright 2009-2017 Jan Niklas Hasse <jhasse@gmail.com>
+// Copyright 2009-2018 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #pragma once
@@ -7,9 +7,11 @@
 #include "jngl/sprite.hpp"
 
 namespace jngl {
-	Finally loadSprite(const std::string&);
-	Sprite& GetSprite(const std::string& filename, Sprite::LoadType loadType = Sprite::LoadType::NORMAL);
 
-	extern unsigned char spriteColorRed, spriteColorGreen, spriteColorBlue, spriteColorAlpha,
-	                     colorRed,       colorGreen,       colorBlue,       colorAlpha;
-}
+Finally loadSprite(const std::string&);
+Sprite& GetSprite(const std::string& filename, Sprite::LoadType loadType = Sprite::LoadType::NORMAL);
+
+extern unsigned char spriteColorRed, spriteColorGreen, spriteColorBlue, spriteColorAlpha,
+                     colorRed,       colorGreen,       colorBlue,       colorAlpha;
+
+} // namespace jngl
