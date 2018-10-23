a3ade0af326740a4743fd2eff3b0b1c44cd48b21
Jan Niklas Hasse <jhasse@bixense.com>
2018-10-23 13:06:23 +0200

    Remove leftovers from GP2X Wiz support

--- src/jngl/input.hpp
+++ src/jngl/input.hpp
@@ -49,20 +49,6 @@ namespace jngl {
 			F10,
 			F11,
 			F12,
-			WizUp,
-			WizDown,
-			WizLeft,
-			WizRight,
-			WizA,
-			WizB,
-			WizX,
-			WizY,
-			WizL,
-			WizR,
-			WizMenu,
-			WizSelect,
-			WizVolUp,
-			WizVolDown,
 			Any
 		};
 	}
