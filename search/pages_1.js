5df31888a61b8bca3950c68799304ad9b1573863
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-23 23:53:45 +0000

    Deploy jhasse/jngl to github.com/jhasse/jngl.git:gh-pages

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['just_20a_20neat_20game_20library',['Just a Neat Game Library',['../md__home_travis_build_jhasse_jngl_doc_index.html',1,'']]]
+  ['just_20a_20neat_20game_20library',['Just a Neat Game Library',['../index.html',1,'']]]
 ];
---
+++ search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "acdfghijkmoqrstvw",
+  1: "cdfjstvw",
+  2: "j",
+  3: "aijostvw",
+  4: "acdghmoqrsv",
+  5: "akt",
+  6: "dj"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "enums",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Enumerations",
+  6: "Pages"
+};
+
