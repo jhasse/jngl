97a23b0cdef1e90d77046e82c9d85624241b3588
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-27 19:22:16 +0000

    Deploy jhasse/jngl to github.com/jhasse/jngl.git:gh-pages

--- search/functions_2.js
+++ search/functions_2.js
@@ -3,5 +3,6 @@ var searchData=
   ['down',['down',['../classjngl_1_1Controller.html#a17997332e2f8c247017cb4315b2646bf',1,'jngl::Controller']]],
   ['draw',['draw',['../classjngl_1_1Job.html#a2bf282904f7854c8a4fd515107aa8c4e',1,'jngl::Job']]],
   ['drawclipped',['drawClipped',['../classjngl_1_1Sprite.html#a18cf7e38a4361af2aeb8ef8ac45b960f',1,'jngl::Sprite::drawClipped(float xstart, float xend, float ystart, float yend) const '],['../classjngl_1_1Sprite.html#a9b68150c27b9402a4b37486a7ebfad3a',1,'jngl::Sprite::drawClipped(Vec2 start, Vec2 end) const ']]],
-  ['drawmesh',['drawMesh',['../classjngl_1_1Sprite.html#a6a7a9d8f762b59bd7209eefcbb986e65',1,'jngl::Sprite']]]
+  ['drawmesh',['drawMesh',['../classjngl_1_1Sprite.html#a6a7a9d8f762b59bd7209eefcbb986e65',1,'jngl::Sprite']]],
+  ['drawscaled',['drawScaled',['../classjngl_1_1Sprite.html#acbe7836ed4b555d32e10b066897e0c98',1,'jngl::Sprite']]]
 ];
