2375d9714e7fd807b80b93a88c205c79ef034e66
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-12 13:08:30 +0000

    Deploy jhasse/jngl to github.com/jhasse/jngl.git:gh-pages

--- search/all_1.js
+++ search/all_1.js
@@ -3,5 +3,6 @@ var searchData=
   ['cancelquit',['cancelQuit',['../namespacejngl.html#a023daf9767d1e2db5d032856dda0b8fa',1,'jngl']]],
   ['color',['Color',['../classjngl_1_1Color.html',1,'jngl']]],
   ['color_2ehpp',['color.hpp',['../color_8hpp.html',1,'']]],
+  ['context',['Context',['../structjngl_1_1ShaderProgram_1_1Context.html',1,'jngl::ShaderProgram']]],
   ['controller',['Controller',['../classjngl_1_1Controller.html',1,'jngl']]]
 ];
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['color',['Color',['../classjngl_1_1Color.html',1,'jngl']]],
+  ['context',['Context',['../structjngl_1_1ShaderProgram_1_1Context.html',1,'jngl::ShaderProgram']]],
   ['controller',['Controller',['../classjngl_1_1Controller.html',1,'jngl']]]
 ];
