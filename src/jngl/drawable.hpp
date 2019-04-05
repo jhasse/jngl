8650ce0cef07e77cb9247c699bc99f92506f451e
Jan Niklas Hasse <jhasse@bixense.com>
2019-04-05 17:40:31 +0200

    Test jngl::Drawable in SpriteTest

--- src/jngl/drawable.hpp
+++ src/jngl/drawable.hpp
@@ -1,4 +1,4 @@
-// Copyright 2012-2018 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2019 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #pragma once
@@ -8,53 +8,58 @@
 #include "dll.hpp"
 
 namespace jngl {
-	class JNGLDLL_API Drawable {
-	public:
-		Drawable();
-		Drawable(const Drawable&) = default;
-		virtual ~Drawable();
-		virtual void step() = 0;
-		virtual void draw() const = 0;
+class JNGLDLL_API Drawable {
+public:
+	Drawable();
+	Drawable(const Drawable&) = default;
+	virtual ~Drawable();
+	virtual void step() = 0;
+	virtual void draw() const = 0;
 
-		virtual void setPos(Float x, Float y);
+	virtual void setPos(Float x, Float y);
 
-		template<class Vect>
-		Vect getCenter() const {
-			return Vect(getX() + getWidth()/2, getY() + getHeight()/2);
-		}
+	template <class Vect> Vect getCenter() const {
+		return Vect(getX() + getWidth() / 2, getY() + getHeight() / 2);
+	}
 
-		virtual void setCenter(Float x, Float y);
+	virtual void setCenter(Float x, Float y);
 
-		template<class Vect>
-		void setCenter(Vect c) {
-			setCenter(c.x, c.y);
-		}
+	template <class Vect> void setCenter(Vect c) {
+		setCenter(c.x, c.y);
+	}
 
-		Float getLeft() const;
-		void setLeft(Float x);
+	/// Returns the distance from the left side of the screen
+	Float getLeft() const;
+	void setLeft(Float x);
 
-		Float getTop() const;
-		void setTop(Float y);
+	/// Returns the distance from the top of the screen
+	Float getTop() const;
+	void setTop(Float y);
 
-		Float getRight() const;
-		void setRight(Float x);
+	/// Returns the distance from the right side of the screen
+	Float getRight() const;
+	void setRight(Float x);
 
-		Float getBottom() const;
-		void setBottom(Float y);
+	/// Returns the distance from the bottom of the screen.
+	Float getBottom() const;
+	void setBottom(Float y);
 
-		Float getX() const;
-		void setX(Float);
+	Float getX() const;
+	void setX(Float);
 
-		Float getY() const;
-		void setY(Float);
+	Float getY() const;
+	void setY(Float);
 
-		float getWidth() const;
-		float getHeight() const;
+	/// Returns the width in screen coordinates
+	float getWidth() const;
 
-	protected:
-		Float x;
-		Float y;
-		float width;
-		float height;
-	};
-}
+	/// Returns the height in screen coordinates
+	float getHeight() const;
+
+protected:
+	Float x;
+	Float y;
+	float width;
+	float height;
+};
+} // namespace jngl
--- src/unittest/SpriteTest.cpp
+++ src/unittest/SpriteTest.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2018-2019 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "../jngl/sprite.hpp"
@@ -7,8 +7,9 @@
 #include <jngl.hpp>
 
 #include <boost/test/unit_test.hpp>
+#include <boost/qvm/vec_operations.hpp>
 
-BOOST_AUTO_TEST_CASE(SpriteTest) {
+BOOST_AUTO_TEST_CASE(Sprite) {
 	for (float factor : { 1.f, 2.f, 3.4f }) {
 		Fixture f(factor);
 		jngl::drawScaled("../jngl.webp", -60, -30, 0.2f);
@@ -22,5 +23,20 @@ BOOST_AUTO_TEST_CASE(SpriteTest) {
 ▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓
 )");
 		jngl::load("../jngl.webp"); // This shouldn't crash
+	}
+	{
+		Fixture f(3.4f);
+		jngl::Sprite sprite("../jngl.webp");
+		BOOST_CHECK_CLOSE(sprite.getWidth(), 600, 1e-9);
+		BOOST_CHECK_CLOSE(sprite.getHeight(), 300, 1e-9);
+		sprite.setCenter(7.3f, 9.1f);
+		BOOST_CHECK_CLOSE(boost::qvm::mag(sprite.getCenter<jngl::Vec2>() - jngl::Vec2(7.3f, 9.1f)),
+		                  0, 1e-5);
+		BOOST_CHECK_CLOSE(sprite.getX(), -292.7f, 1e-5);
+		BOOST_CHECK_CLOSE(sprite.getY(), -140.9f, 1e-5);
+		BOOST_CHECK_CLOSE(sprite.getLeft(), -132.7f, 1e-5);
+		BOOST_CHECK_CLOSE(sprite.getTop(), -105.9f, 1e-5);
+		BOOST_CHECK_CLOSE(sprite.getRight(), -147.3f, 1e-5);
+		BOOST_CHECK_CLOSE(sprite.getBottom(), -124.1f, 1e-5);
 	}
 }
