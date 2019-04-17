cd0eb0cc60cd2bad4ae3ce632695c4fc1f642846
Jan Niklas Hasse <jhasse@bixense.com>
2019-04-17 10:05:56 +0200

    Android: Empty implementation for getConnectedControllers

---
+++ src/android/input.cpp
@@ -0,0 +1,12 @@
+// Copyright 2019 Jan Niklas Hasse <jhasse@bixense.com>
+// For conditions of distribution and use, see copyright notice in LICENSE.txt
+
+#include "../jngl/input.hpp"
+
+namespace jngl {
+
+std::vector<std::shared_ptr<Controller>> getConnectedControllers() {
+	return {}; // not implemented yet
+}
+
+} // namespace jngl
