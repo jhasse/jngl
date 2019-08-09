d25ac9e9cf49533b09e7c6f2f0510f7079cb2fe9
Jan Niklas Hasse <jhasse@bixense.com>
2019-08-09 15:50:50 +0200

    Use correct include path for debug.hpp

--- src/ios/windowimpl.cpp
+++ src/ios/windowimpl.cpp
@@ -1,21 +1,21 @@
-// Copyright 2012-2018 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2019 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "windowimpl.hpp"
 
 #include "../window.hpp"
-#include "debug.hpp"
+#include "../jngl/debug.hpp"
 
 namespace jngl {
 	WindowImpl::WindowImpl(Window* w) : window(w) {
 		debug("init with Window: "); debug(window); debug(" this: "); debug(this); debug("\n");
 	}
-	
+
 	void WindowImpl::setMouse(int x, int y) {
 		mouseX = x;
 		mouseY = y;
 	}
-	
+
 	void WindowImpl::setMouseDown(bool d) {
 		if (window->mouseDown_.at(jngl::mouse::Left)) {
 			window->multitouch = d;
@@ -23,7 +23,7 @@ namespace jngl {
 		window->mousePressed_.at(jngl::mouse::Left) = d;
 		window->mouseDown_.at(jngl::mouse::Left) = d;
 	}
-	
+
 	void WindowImpl::setRelativeMouseMode(bool r) {
 		if (r) {
 			relativeX = window->mousex_;
@@ -34,7 +34,7 @@ namespace jngl {
 			relativeX = relativeY = 0;
 		}
 	}
-	
+
 	void WindowImpl::updateInput() {
 		window->mousex_ = mouseX - relativeX;
 		window->mousey_ = mouseY - relativeY;
