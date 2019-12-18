0ccaab8b2bf1cf82e5a0b11ac3ebdd276271b0aa
Jan Niklas Hasse <jhasse@bixense.com>
2019-12-18 22:26:53 +0100

    Use correct casts in Line::setFont
    Also format src/jngl/text.cpp

--- src/jngl/text.cpp
+++ src/jngl/text.cpp
@@ -1,4 +1,4 @@
-// Copyright 2012-2018 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2019 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "text.hpp"
@@ -11,85 +11,86 @@
 #include "screen.hpp"
 
 namespace jngl {
-	class Line : public jngl::Drawable {
-	public:
-		Line(std::string text, std::shared_ptr<FontImpl> font) : text(std::move(text)) {
-			setFont(std::move(font));
-		}
-		void setFont(std::shared_ptr<FontImpl> font) {
-			width = static_cast<int>(font->getTextWidth(text) * getScaleFactor());
-			height = font->getLineHeight();
-			this->font = std::move(font);
-		}
-		void step() override {
-		}
-		void draw() const override {
-			font->print(int(x / getScaleFactor()), int(y / getScaleFactor()), text);
-		}
-	private:
-		std::string text;
-		std::shared_ptr<FontImpl> font;
-	};
-
-	Text::Text(const std::string& text) : font(pWindow->getFontImpl()) {
-		setText(text);
+class Line : public jngl::Drawable {
+public:
+	Line(std::string text, std::shared_ptr<FontImpl> font) : text(std::move(text)) {
+		setFont(std::move(font));
+	}
+	void setFont(std::shared_ptr<FontImpl> font) {
+		width = font->getTextWidth(text) * getScaleFactor();
+		height = static_cast<float>(font->getLineHeight());
+		this->font = std::move(font);
+	}
+	void step() override {
+	}
+	void draw() const override {
+		font->print(int(x / getScaleFactor()), int(y / getScaleFactor()), text);
 	}
 
-	void Text::setText(const std::string& text) {
-		lines.clear();
-		for (const auto& lineText : splitlines(text)) {
-			auto newLine = std::make_shared<Line>(lineText, font);
-			lines.emplace_back(std::move(newLine));
-		}
-		setAlign(align);
-	}
+private:
+	std::string text;
+	std::shared_ptr<FontImpl> font;
+};
 
-	void Text::setFont(Font& f) {
-		font = f.getImpl();
-		for (auto& line : lines) {
-			line->setFont(font);
-		}
-		setAlign(align);
-	}
+Text::Text(const std::string& text) : font(pWindow->getFontImpl()) {
+	setText(text);
+}
 
-	void Text::setAlign(Alignment a) {
-		align = a;
-		// Recalculate max width
-		width = 0;
-		for (const auto& line : lines) {
-			if (line->getWidth() > width) {
-				width = line->getWidth();
-			}
-		}
-		height = 0;
-		for (auto& line : lines) {
-			switch (align) {
-			case Alignment::LEFT:
-				line->setX(0);
-				break;
-			case Alignment::CENTER:
-				line->setX((width - line->getWidth()) / 2.);
-				break;
-			case Alignment::RIGHT:
-				line->setX(width - line->getWidth());
-				break;
-			}
-			line->setY(height);
-			height += line->getHeight();
-		}
-		width *= getScaleFactor();
-		height *= getScaleFactor();
+void Text::setText(const std::string& text) {
+	lines.clear();
+	for (const auto& lineText : splitlines(text)) {
+		auto newLine = std::make_shared<Line>(lineText, font);
+		lines.emplace_back(std::move(newLine));
 	}
+	setAlign(align);
+}
 
-	void Text::step() {
+void Text::setFont(Font& f) {
+	font = f.getImpl();
+	for (auto& line : lines) {
+		line->setFont(font);
 	}
+	setAlign(align);
+}
 
-	void Text::draw() const {
-		jngl::pushMatrix();
-		jngl::translate(int(x / getScaleFactor()), int(y / getScaleFactor()));
-		for (auto& line : lines) {
-			line->draw();
+void Text::setAlign(Alignment a) {
+	align = a;
+	// Recalculate max width
+	width = 0;
+	for (const auto& line : lines) {
+		if (line->getWidth() > width) {
+			width = line->getWidth();
 		}
-		jngl::popMatrix();
 	}
+	height = 0;
+	for (auto& line : lines) {
+		switch (align) {
+		case Alignment::LEFT:
+			line->setX(0);
+			break;
+		case Alignment::CENTER:
+			line->setX((width - line->getWidth()) / 2.);
+			break;
+		case Alignment::RIGHT:
+			line->setX(width - line->getWidth());
+			break;
+		}
+		line->setY(height);
+		height += line->getHeight();
+	}
+	width *= getScaleFactor();
+	height *= getScaleFactor();
+}
+
+void Text::step() {
+}
+
+void Text::draw() const {
+	jngl::pushMatrix();
+	jngl::translate(int(x / getScaleFactor()), int(y / getScaleFactor()));
+	for (auto& line : lines) {
+		line->draw();
+	}
+	jngl::popMatrix();
+}
 } // namespace jngl
