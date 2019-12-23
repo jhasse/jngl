dea2cdaec7fc8abff52bbe841989b647e92f483a
Jan Niklas Hasse <jhasse@bixense.com>
2019-12-23 16:33:58 +0100

    Add Finally.hpp to doxygen

--- src/jngl/Finally.hpp
+++ src/jngl/Finally.hpp
@@ -1,9 +1,7 @@
 // Copyright 2007-2019 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
-
+/// @file
 #pragma once
-
-#include "dll.hpp"
 
 #include <functional>
 
@@ -12,12 +10,13 @@ namespace jngl {
 /// Helper class which calls a function when being destroyed
 class Finally {
 public:
-	JNGLDLL_API explicit Finally(std::function<void()> functionToCall);
-	JNGLDLL_API Finally(Finally&&) noexcept;
+	explicit Finally(std::function<void()> functionToCall);
+	Finally(Finally&&) noexcept;
 	Finally& operator=(Finally&&) noexcept;
 	Finally(const Finally&) = delete;
 	Finally& operator=(const Finally&) = delete;
-	JNGLDLL_API ~Finally();
+	~Finally();
+
 private:
 	std::function<void()> functionToCall;
 };
