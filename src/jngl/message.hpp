8ebbab5c7e3fef83bb150c4e427fcd39c8cd90d7
Jan Niklas Hasse <jhasse@bixense.com>
2020-01-13 18:57:11 +0100

    Add documentation for message.hpp

--- src/jngl/message.hpp
+++ src/jngl/message.hpp
@@ -1,16 +1,19 @@
-// Copyright 2015 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2015-2020 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
-
+/// @file
 #pragma once
-
-#include "dll.hpp"
 
 #include <string>
 
 namespace jngl {
 
-void JNGLDLL_API errorMessage(const std::string& text);
+/// Shows a message box with an error \a text
+void errorMessage(const std::string& text);
 
-void JNGLDLL_API printMessage(const std::string& text);
+/// Prints \a text on stdout
+///
+/// Normally this is the same as `std::cout << text << std::flush;`. On Android this will use
+/// `__android_log_print` for example.
+void printMessage(const std::string& text);
 
 } // namespace jngl
