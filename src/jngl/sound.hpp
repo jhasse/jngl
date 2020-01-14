a0db77c5b8435bdbe067e9a3da8cb70179dbe8bc
Jan Niklas Hasse <jhasse@bixense.com>
2020-01-14 23:10:10 +0100

    Remove JNGLDLL_API from sound.hpp

--- src/jngl/sound.hpp
+++ src/jngl/sound.hpp
@@ -1,32 +1,30 @@
-// Copyright 2012-2019 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2020 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #pragma once
-
-#include "dll.hpp"
 
 #include <string>
 
 namespace jngl {
 
-float JNGLDLL_API getVolume();
+float getVolume();
 
 /// Play an OGG audio file once
-void JNGLDLL_API play(const std::string& filename);
+void play(const std::string& filename);
 
 /// Stop an OGG audio file if it's currently playing
-void JNGLDLL_API stop(const std::string& filename);
+void stop(const std::string& filename);
 
 /// \return whether \a filename is currently playing
-bool JNGLDLL_API isPlaying(const std::string& filename);
+bool isPlaying(const std::string& filename);
 
 /// Play an OGG audio file in a loop
-void JNGLDLL_API loop(const std::string& filename);
+void loop(const std::string& filename);
 
-bool JNGLDLL_API isOpenALInstalled();
+bool isOpenALInstalled();
 
-void JNGLDLL_API setPlaybackSpeed(float speed);
+void setPlaybackSpeed(float speed);
 
-void JNGLDLL_API setVolume(float volume);
+void setVolume(float volume);
 
 } // namespace jngl
