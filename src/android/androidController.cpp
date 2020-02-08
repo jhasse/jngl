2c23adc3c91b2568ef2fcd0594a40f9694f8e84b
Kolja Lubitz <pinguin999@gmail.com>
2020-02-09 00:33:57 +0100

    ci

--- src/android/androidController.cpp
+++ src/android/androidController.cpp
@@ -1,7 +1,7 @@
 // Copyright 2017-2019 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
-#include "AndroidController.hpp"
+#include "asndroidController.hpp"
 
 #include "../jngl/debug.hpp"
 
