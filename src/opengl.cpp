c5c78c7b00c4ad60eb60c2e2c0c0f63d57c76188
Jan Niklas Hasse <jhasse@bixense.com>
2020-04-11 17:18:12 +0200

    Make App::isPixelArt static to avoid exception

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -29,13 +29,8 @@ GLuint genAndBindTexture() {
 	GLuint texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	try {
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-		                jngl::App::instance().isPixelArt() ? GL_NEAREST : GL_LINEAR);
-	} catch (std::runtime_error&) {
-		// TODO: Don't ignore an exception
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	}
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+	                jngl::App::isPixelArt() ? GL_NEAREST : GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
