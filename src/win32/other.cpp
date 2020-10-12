967a7e479457bda938bee5ea158d613dd5023d7e
Jan Niklas Hasse <jhasse@bixense.com>
2020-10-12 15:24:57 +0200

    Implement getSystemConfigPath for UWP

--- src/win32/other.cpp
+++ src/win32/other.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2018-2020 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "../jngl/other.hpp"
@@ -8,6 +8,11 @@
 #include <shlobj.h>
 #include <stdexcept>
 #include <windows.h>
+
+#ifdef JNGL_UWP
+#include <windows.storage.h>
+#include <wrl.h>
+#endif
 
 namespace jngl {
 
@@ -20,7 +25,40 @@ std::string getBinaryPath() {
 
 std::string getSystemConfigPath() {
 #if JNGL_UWP
-	return {}; // TODO
+	// https://stackoverflow.com/a/39825568/647898
+	using namespace ABI::Windows::Storage;
+	using namespace Microsoft::WRL;
+	using namespace Microsoft::WRL::Wrappers;
+
+	ComPtr<IApplicationDataStatics> appDataStatics;
+	auto hr = RoGetActivationFactory(HStringReference(L"Windows.Storage.ApplicationData").Get(),
+	                                 __uuidof(appDataStatics), &appDataStatics);
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to retrieve application data statics");
+	}
+	ComPtr<IApplicationData> appData;
+	hr = appDataStatics->get_Current(&appData);
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to retrieve current application data");
+	}
+	ComPtr<IStorageFolder> roamingFolder;
+	hr = appData->get_RoamingFolder(&roamingFolder);
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to retrieve roaming folder");
+	}
+	ComPtr<IStorageItem> folderItem;
+	hr = roamingFolder.As(&folderItem);
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to cast roaming folder to IStorageItem");
+	}
+	HString roamingPathHString;
+	hr = folderItem->get_Path(roamingPathHString.GetAddressOf());
+	if (FAILED(hr)) {
+		throw std::runtime_error("Failed to retrieve roaming folder path");
+	}
+	uint32_t pathLength;
+	auto roamingPathCStr = roamingPathHString.GetRawBuffer(&pathLength);
+	return utf16ToUtf8(std::wstring(roamingPathCStr, pathLength));
 #else
 	PWSTR pPath;
 	if (SHGetKnownFolderPath(FOLDERID_RoamingAppData, CSIDL_FLAG_CREATE, nullptr, &pPath) != S_OK) {
