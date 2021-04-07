c0b2f0c195d396f36d31136e5e881932687b0c70
Jan Niklas Hasse <jhasse@bixense.com>
2021-04-07 15:07:31 +0200

    #include <algorithm> for MSVC

--- src/jngl/font.cpp
+++ src/jngl/font.cpp
@@ -1,10 +1,11 @@
-// Copyright 2012-2020 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2021 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "font.hpp"
 
 #include "../freetype.hpp"
 
+#include <algorithm>
 #include <stack>
 
 namespace jngl {
