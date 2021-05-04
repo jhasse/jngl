24aeb8afba4e4c1150a0e5fcfdbf6454c5bcf26c
jhasse <jhasse@bixense.com>
2021-05-04 15:55:03 +0000

    Deploying to gh-pages from  @ 81087cd1a724516873591c76f3aaaeda4ff942a2 🚀

--- search/all_a.js
+++ search/all_a.js
@@ -1,12 +1,11 @@
 var searchData=
 [
-  ['jmp_5fbuf_1118',['jmp_buf',['http://en.cppreference.com/w/cpp/utility/program/jmp_buf.html',1,'std']]],
-  ['jngl_1119',['jngl',['../namespacejngl.html',1,'']]],
-  ['jngl_2ehpp_1120',['jngl.hpp',['../jngl_8hpp.html',1,'']]],
-  ['jnglinit_1121',['jnglInit',['../init_8hpp.html#a159a465a80aa2a9076328fba10a1c95d',1,'init.hpp']]],
-  ['job_1122',['Job',['../classjngl_1_1Job.html',1,'Job'],['../classjngl_1_1Job.html#ad3919dba75c4130c8b6dd0ab31e9a461',1,'jngl::Job::Job()=default'],['../classjngl_1_1Job.html#af6013bfb11fc1160c0206d5ac0ab0902',1,'jngl::Job::Job(const Job &amp;)=default'],['../classjngl_1_1Job.html#afa9ccc3a57c690756a2f2aa26d785f1c',1,'jngl::Job::Job(Job &amp;&amp;)=default']]],
-  ['job_2ehpp_1123',['job.hpp',['../job_8hpp.html',1,'']]],
-  ['join_1124',['join',['http://en.cppreference.com/w/cpp/thread/thread/join.html',1,'std::thread']]],
-  ['joinable_1125',['joinable',['http://en.cppreference.com/w/cpp/thread/thread/joinable.html',1,'std::thread']]],
-  ['just_20a_20neat_20game_20library_1126',['Just a Neat Game Library',['../index.html',1,'']]]
+  ['jmp_5fbuf_1119',['jmp_buf',['http://en.cppreference.com/w/cpp/utility/program/jmp_buf.html',1,'std']]],
+  ['jngl_1120',['jngl',['../namespacejngl.html',1,'']]],
+  ['jngl_2ehpp_1121',['jngl.hpp',['../jngl_8hpp.html',1,'']]],
+  ['jnglinit_1122',['jnglInit',['../init_8hpp.html#a159a465a80aa2a9076328fba10a1c95d',1,'init.hpp']]],
+  ['job_1123',['Job',['../classjngl_1_1Job.html',1,'Job'],['../classjngl_1_1Job.html#ad3919dba75c4130c8b6dd0ab31e9a461',1,'jngl::Job::Job()=default'],['../classjngl_1_1Job.html#af6013bfb11fc1160c0206d5ac0ab0902',1,'jngl::Job::Job(const Job &amp;)=default'],['../classjngl_1_1Job.html#afa9ccc3a57c690756a2f2aa26d785f1c',1,'jngl::Job::Job(Job &amp;&amp;)=default']]],
+  ['job_2ehpp_1124',['job.hpp',['../job_8hpp.html',1,'']]],
+  ['join_1125',['join',['http://en.cppreference.com/w/cpp/thread/thread/join.html',1,'std::thread']]],
+  ['joinable_1126',['joinable',['http://en.cppreference.com/w/cpp/thread/thread/joinable.html',1,'std::thread']]]
 ];
--- search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['just_20a_20neat_20game_20library_5148',['Just a Neat Game Library',['../index.html',1,'']]]
-];
