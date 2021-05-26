46679119b2e0ade4c27d84bec473c33b73ed2913
Jan Niklas Hasse <jhasse@bixense.com>
2021-05-26 21:27:09 +0200

    Support older SDL2 versions without SDL_GetPreferredLocales

--- src/sdl/other.cpp
+++ src/sdl/other.cpp
@@ -3,16 +3,20 @@
 
 #include "../jngl/other.hpp"
 
-#include <SDL.h>
+#if defined(__has_include) && __has_include(<SDL_locale.h>)
+#include <SDL_locale.h>
+#endif
 
 namespace jngl {
 
 std::string getPreferredLanguage() {
+#if defined(__has_include) && __has_include(<SDL_locale.h>)
 	SDL_Locale* locale = SDL_GetPreferredLocales();
 	if (locale && locale->language && locale->language[0] != '\0' && locale->language[1] != '\0' &&
 	    locale->language[2] == '\0') {
 		return locale->language;
 	}
+#endif
 	return "en";
 }
 
