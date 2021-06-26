68f2416c06ef20a61e6bfa894c388bcea2e65c61
Jan Niklas Hasse <jhasse@bixense.com>
2021-06-26 20:04:31 +0200

    Add documentation for SoundFile

--- src/jngl/SoundFile.hpp
+++ src/jngl/SoundFile.hpp
@@ -16,17 +16,30 @@ struct SoundParams;
 /// Sound loaded from an OGG file
 class SoundFile {
 public:
+	/// Load an OGG file called \a filename
 	explicit SoundFile(const std::string& filename);
 	~SoundFile();
 	SoundFile(const SoundFile&) = delete;
 	SoundFile& operator=(const SoundFile&) = delete;
 	SoundFile(SoundFile&&) noexcept;
 	SoundFile& operator=(SoundFile&&) noexcept;
+
+	/// Play the sound once. If called twice the sound would also play twice
 	void play();
+
+	/// Stop the last started sound
 	void stop();
+
+	/// Whether the sound is still playing at least once
 	bool isPlaying();
+
+	/// Play the sound in a loop. Can also be stopped using stop()
 	void loop();
-	void setPitch(float p);
+
+	/// Set pitch in (0.0f, ∞]. Default is 1.0f
+	void setPitch(float);
+
+	/// Set volume in [0, ∞]. Default is 1.0f
 	void setVolume(float v);
 
 private:
