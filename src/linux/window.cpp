80bee20adf8469ff5ab8711bf2e13d04dad24510
Jan Niklas Hasse <jhasse@bixense.com>
2021-06-29 21:56:42 +0200

    Fix memory leak of FcFontSet* on Linux

--- src/linux/window.cpp
+++ src/linux/window.cpp
@@ -1,7 +1,9 @@
-// Copyright 2007-2020 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2007-2021 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "../window.hpp"
+
+#include "../jngl/Finally.hpp"
 
 #include <fontconfig/fontconfig.h>
 #include <stdexcept>
@@ -11,6 +13,7 @@ namespace jngl {
 std::string Window::GetFontFileByName(const std::string& fontname) {
 	const double size = 12;
 	FcFontSet* fontSet = FcFontSetCreate();
+	Finally _([fontSet]() { FcFontSetDestroy(fontSet); });
 
 	FcPattern* pattern = FcPatternBuild(nullptr, FC_FAMILY, FcTypeString, fontname.c_str(), FC_SIZE,
 	                                    FcTypeDouble, size, nullptr);
@@ -22,7 +25,6 @@ std::string Window::GetFontFileByName(const std::string& fontname) {
 	FcPatternDestroy(pattern);
 
 	if (!match) {
-		FcFontSetDestroy(fontSet);
 		throw std::runtime_error(std::string("Couldn't load " + fontname));
 	}
 	FcFontSetAdd(fontSet, match);
