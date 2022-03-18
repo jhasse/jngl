59805f3d758fb8926c2fce6dca78f6d09d4b38a8
Jan Niklas Hasse <jhasse@bixense.com>
2022-03-18 17:04:35 +0100

    Deprecate jngl::updateInput

--- src/jngl/other.hpp
+++ src/jngl/other.hpp
@@ -1,4 +1,4 @@
-// Copyright 2014-2021 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2014-2022 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 /// Miscellaneous functions
 /// @file
@@ -14,7 +14,9 @@ namespace jngl {
 bool running();
 
 /// Updates the input state. App::mainLoop() calls this before Work::step()
-void updateInput();
+///
+/// \deprecated Do not call this yourself, use jngl::Work instead
+[[deprecated("Do not call this yourself, use jngl::Work instead")]] void updateInput();
 
 /// Swaps back and front buffer. App::mainLoop() calls this after Work::draw()
 ///
