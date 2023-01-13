2f45b78e3b19711d3cb72819c6e5f09088068ecd
Jan Niklas Hasse <jhasse@bixense.com>
2023-01-13 17:13:36 +0100

    Add a dummy backend to SDL2 audio::engine for devices without a sound card

--- src/audio/sdl/engine.cpp
+++ src/audio/sdl/engine.cpp
@@ -1,4 +1,6 @@
 #include "../engine.hpp"
+
+#include "../../jngl/debug.hpp"
 #include "../constants.hpp"
 
 #include <SDL.h>
@@ -9,84 +11,141 @@
 #include <vector>
 #include <optional>
 #include <cmath>
+#include <thread>
 
 namespace psemek::audio
 {
 
-	struct engine::Impl
-	{
+struct engine::Impl {
+	Impl(std::shared_ptr<stream> output) {
+		try {
+			backend = std::make_unique<SdlImpl>(output);
+		} catch (std::exception& e) {
+			jngl::debugLn(e.what());
+			backend = std::make_unique<DummyImpl>(std::move(output));
+		}
+	}
+	struct Backend {
+		virtual ~Backend() = default;
+		virtual void setPause(bool) = 0;
+	};
+	std::unique_ptr<Backend> backend;
+
+	struct DummyImpl : public Backend {
+		DummyImpl(std::shared_ptr<stream> output)
+		: output(std::move(output)), thread(
+		                                 [this]()
+		                                 {
+			const int slowdown = 10;
+			std::array<float, frequency / slowdown * 2 /* stereo */> buffer;
+			while (!quit) {
+				do {
+					std::this_thread::sleep_for(std::chrono::milliseconds(1000 / slowdown));
+				} while (pause && !quit);
+				const auto read = this->output->read(buffer.data(), buffer.size());
+				assert(read == buffer.size());
+				// to debug sound on the terminal:
+				// float average = 0;
+				// for (float f : buffer) {
+				// 	average += std::fabs(f / buffer.size());
+				// }
+				// if (average < 0.1f) {
+				// 	jngl::debug(' ');
+				// } else if (average < 0.2f) {
+				// 	jngl::debug("▁");
+				// } else if (average < 0.3f) {
+				// 	jngl::debug("▂");
+				// } else if (average < 0.4f) {
+				// 	jngl::debug("▃");
+				// } else if (average < 0.5f) {
+				// 	jngl::debug("▄");
+				// } else if (average < 0.6f) {
+				// 	jngl::debug("▅");
+				// } else if (average < 0.7f) {
+				// 	jngl::debug("▆");
+				// } else if (average < 0.8f) {
+				// 	jngl::debug("▇");
+				// } else {
+				// 	jngl::debug("█");
+				// }
+			}
+		  }) {
+		}
+		~DummyImpl() {
+			quit = true;
+			thread.join();
+		}
+		void setPause(bool pause) override {
+			this->pause = pause;
+		}
+
+		std::shared_ptr<stream> output;
+		std::atomic_bool pause{ false };
+		std::atomic_bool quit{ false };
+		std::thread thread;
+	};
+
+	struct SdlImpl : public Backend {
 		std::shared_ptr<void> sdl_init;
 
 		SDL_AudioDeviceID device;
 
 		std::vector<float> buffer;
-		bool thread_registered = false;
 
 		std::shared_ptr<stream> output;
 
-		Impl(std::shared_ptr<stream>);
-		~Impl();
-
-		static void callback(void * userdata, std::uint8_t * stream, int len);
-	};
-
-	engine::Impl::Impl(std::shared_ptr<stream> output) : output(std::move(output)) {
-		if (SDL_Init(SDL_INIT_AUDIO) < 0) {
-			throw std::runtime_error(SDL_GetError());
-		}
-		SDL_AudioSpec desired, obtained;
-		desired.freq = frequency;
-		desired.channels = 2;
-		desired.format = AUDIO_S16SYS;
-		desired.samples = 256;
-		desired.callback = &callback;
-		desired.userdata = this;
-		if (device = SDL_OpenAudioDevice(nullptr, 0, &desired, &obtained, 0); device == 0) {
-			assert(false);
-			// sdl2::fail("SDL_OpenAudioDevice failed:"); TODO
-		}
+		SdlImpl(std::shared_ptr<stream> output) : output(std::move(output)) {
+			if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+				throw std::runtime_error(SDL_GetError());
+			}
+			SDL_AudioSpec desired, obtained;
+			desired.freq = frequency;
+			desired.channels = 2;
+			desired.format = AUDIO_S16SYS;
+			desired.samples = 256;
+			desired.callback = &callback;
+			desired.userdata = this;
+			if (device = SDL_OpenAudioDevice(nullptr, 0, &desired, &obtained, 0); device == 0) {
+				throw std::runtime_error(SDL_GetError());
+			}
 
 		// log::info() << "Initialized audio: " << static_cast<int>(obtained.channels) << " channels, " << obtained.freq << " Hz, " << obtained.samples << " samples";
 
-		buffer.resize(obtained.samples * obtained.channels);
-		SDL_PauseAudioDevice(device, 0);
-	}
-
-	engine::Impl::~Impl()
-	{
-		SDL_CloseAudioDevice(device);
-	}
-
-	void engine::Impl::callback(void * userdata, std::uint8_t * dst_u8, int len)
-	{
-		static std::string const profiler_str = "audio";
-		// prof::profiler prof(profiler_str);
-
-	    auto self = static_cast<Impl*>(userdata);
-	    std::int16_t * dst = reinterpret_cast<std::int16_t *>(dst_u8);
-
-		if (!self->thread_registered)
-		{
-			// log::register_thread("audio");
-			self->thread_registered = true;
+			buffer.resize(obtained.samples * obtained.channels);
+			SDL_PauseAudioDevice(device, 0);
+		}
+		~SdlImpl() {
+			SDL_CloseAudioDevice(device);
 		}
 
-		std::size_t const size = len / 2;
-		std::size_t read = 0;
-		read = self->output->read(self->buffer.data(), size);
-		std::fill(self->buffer.data() + read, self->buffer.data() + size, 0.f);
+		static void callback(void* userdata, std::uint8_t* dst_u8, int len) {
+			static std::string const profiler_str = "audio";
+			// prof::profiler prof(profiler_str);
 
-		for (auto s : self->buffer)
-			*dst++ = static_cast<std::int16_t>(std::max(std::min((65535.f * s - 1.f) / 2.f, 32767.f), -32768.f));
-	}
+			auto self = static_cast<SdlImpl*>(userdata);
+			std::int16_t * dst = reinterpret_cast<std::int16_t *>(dst_u8);
 
-	engine::engine(std::shared_ptr<stream> output)
-		: impl(std::make_unique<Impl>(std::move(output)))
-	{}
+			std::size_t const size = len / 2;
+			std::size_t read = 0;
+			read = self->output->read(self->buffer.data(), size);
+			std::fill(self->buffer.data() + read, self->buffer.data() + size, 0.f);
 
-	engine::~engine() = default;
+			for (auto s : self->buffer)
+				*dst++ = static_cast<std::int16_t>(std::max(std::min((65535.f * s - 1.f) / 2.f, 32767.f), -32768.f));
+		}
 
-	void engine::setPause(bool pause) {
-		SDL_PauseAudioDevice(impl->device, pause ? 1 : 0);
-	}
+		void setPause(bool pause) override {
+			SDL_PauseAudioDevice(device, pause ? 1 : 0);
+		}
+	};
+};
+
+engine::engine(std::shared_ptr<stream> output) : impl(std::make_unique<Impl>(std::move(output))) {
+}
+
+engine::~engine() = default;
+
+void engine::setPause(bool pause) {
+	impl->backend->setPause(pause);
+}
 }
