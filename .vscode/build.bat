cb6ece4a17354ee864b4a818309a72b48972d91d
Jan Niklas Hasse <jhasse@bixense.com>
2023-01-31 17:52:10 +0100

    VSCode: Generate build dir if needed on Windows

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -1,2 +1,7 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+if exist build\build.ninja (
+    rem file exists
+) else (
+    cmake -Bbuild -GNinja -DJNGL_VIDEO=0 -DCMAKE_BUILD_TYPE=Debug
+)
 cmake --build build
