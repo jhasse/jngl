fcfd31cb5f99c0ad03300689e4186af0432a6bd9
Jan Niklas Hasse <jhasse@bixense.com>
2023-02-10 21:33:41 +0100

    Add Achievement.hpp to all.hpp

--- src/jngl/all.hpp
+++ src/jngl/all.hpp
@@ -1,9 +1,10 @@
-// Copyright 2012-2022 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2023 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 /// Includes all JNGL headers except for init.hpp
 /// @file
 #pragma once
 
+#include "Achievement.hpp"
 #include "App.hpp"
 #include "Color.hpp"
 #include "Container.hpp"
