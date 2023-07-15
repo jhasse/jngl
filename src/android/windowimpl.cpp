e38f95682c893da46def874cab725551ea600ed8
Jan Niklas Hasse <jhasse@bixense.com>
2023-07-15 11:55:36 +0200

    You should always compile xD

--- src/android/windowimpl.cpp
+++ src/android/windowimpl.cpp
@@ -50,7 +50,7 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
 
 static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
 	if (!app || !app->userData) {
-		return; // shouldn't happen but I seen a crash in this function in the Play Console
+		return 0; // shouldn't happen but I seen a crash in this function in the Play Console
 	}
 	WindowImpl& impl = *reinterpret_cast<WindowImpl*>(app->userData);
 	const auto source = AInputEvent_getSource(event);
