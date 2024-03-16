64b70540ebb199ee396a3cbba5da09c11c509c3c
jhasse <jhasse@bixense.com>
2024-03-16 17:12:56 +0000

    Deploying to gh-pages from @ jhasse/jngl@8603141a3c0b65bdad469be891ecc2465c181730 🚀

--- search/files_e.js
+++ search/files_e.js
@@ -4,5 +4,6 @@ var searchData=
   ['ranges_1',['ranges',['http://en.cppreference.com/w/cpp/header/ranges.html',1,'']]],
   ['ratio_2',['ratio',['http://en.cppreference.com/w/cpp/header/ratio.html',1,'']]],
   ['regex_3',['regex',['http://en.cppreference.com/w/cpp/header/regex.html',1,'']]],
-  ['rgba_2ehpp_4',['Rgba.hpp',['../Rgba_8hpp.html',1,'']]]
+  ['rgb_2ehpp_4',['Rgb.hpp',['../Rgb_8hpp.html',1,'']]],
+  ['rgba_2ehpp_5',['Rgba.hpp',['../Rgba_8hpp.html',1,'']]]
 ];
