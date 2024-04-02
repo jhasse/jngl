efe72e0a69e4d633b3adff374cab257c079f3811
jhasse <jhasse@bixense.com>
2024-04-02 20:01:14 +0000

    Deploying to gh-pages from @ jhasse/jngl@d5893cfc676417b83f2161408ac352f0f28a0b26 🚀

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['first_0',['first',['http://en.cppreference.com/w/cpp/utility/pair.html',1,'std::csub_match::first()'],['http://en.cppreference.com/w/cpp/utility/pair.html',1,'std::pair::first()'],['http://en.cppreference.com/w/cpp/utility/pair.html',1,'std::ssub_match::first()'],['http://en.cppreference.com/w/cpp/utility/pair.html',1,'std::sub_match::first()'],['http://en.cppreference.com/w/cpp/utility/pair.html',1,'std::wcsub_match::first()'],['http://en.cppreference.com/w/cpp/utility/pair.html',1,'std::wssub_match::first()']]],
-  ['free_1',['free',['http://en.cppreference.com/w/cpp/experimental/fs/space_info.html',1,'std::experimental::filesystem::space_info::free()'],['http://en.cppreference.com/w/cpp/filesystem/space_info.html',1,'std::filesystem::space_info::free()']]]
+  ['free_1',['free',['http://en.cppreference.com/w/cpp/experimental/fs/space_info.html',1,'std::experimental::filesystem::space_info::free()'],['http://en.cppreference.com/w/cpp/filesystem/space_info.html',1,'std::filesystem::space_info::free()']]],
+  ['fullscreen_2',['fullscreen',['../structjngl_1_1AppParameters.html#a4e1d87e51100938785fd99460144b866',1,'jngl::AppParameters']]]
 ];
