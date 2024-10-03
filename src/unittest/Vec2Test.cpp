c51cea18ccdfd69906d815424f2f38ada36fd17f
Jan Niklas Hasse <jhasse@bixense.com>
2024-10-03 10:15:39 +0200

    Add unittest for std::format with jngl::Vec2

--- src/unittest/Vec2Test.cpp
+++ src/unittest/Vec2Test.cpp
@@ -10,34 +10,36 @@ boost::ut::suite _ = [] {
 	using namespace boost::ut; // NOLINT
 
 	"Vec2"_test = [] {
-        //
-        //  o-->
-        //
-        jngl::Vec2 a{ 1., 0. };
+		//
+		//  o-->
+		//
+		jngl::Vec2 a{ 1., 0. };
 
-        a.rotate(M_PI / 2);
-        //
-        //  o
-        //  |
-        //  v
-        //
-        expect(approx(a.x, 0., 1e-6));
-        expect(approx(a.y, 1., 1e-6)); // JNGL uses negative y for up and positive for down.
+		expect(eq(std::format("{}", a), std::string("[x=1, y=0]")));
 
-        a.rotate(-M_PI / 4);
-        //
-        //  o
-        //   ╲
-        //    ⌟
-        //
-        expect(approx(a.x, sqrt(2) / 2., 1e-6));
-        expect(approx(a.y, sqrt(2) / 2., 1e-6));
-        a.rotate(0);
-        expect(approx(a.x, sqrt(2) / 2., 1e-6));
-        expect(approx(a.y, sqrt(2) / 2., 1e-6));
-        a.rotate(M_PI * 8);
-        expect(approx(a.x, sqrt(2) / 2., 1e-6));
-        expect(approx(a.y, sqrt(2) / 2., 1e-6));
+		a.rotate(M_PI / 2);
+		//
+		//  o
+		//  |
+		//  v
+		//
+		expect(approx(a.x, 0., 1e-6));
+		expect(approx(a.y, 1., 1e-6)); // JNGL uses negative y for up and positive for down.
+
+		a.rotate(-M_PI / 4);
+		//
+		//  o
+		//   ╲
+		//    ⌟
+		//
+		expect(approx(a.x, sqrt(2) / 2., 1e-6));
+		expect(approx(a.y, sqrt(2) / 2., 1e-6));
+		a.rotate(0);
+		expect(approx(a.x, sqrt(2) / 2., 1e-6));
+		expect(approx(a.y, sqrt(2) / 2., 1e-6));
+		a.rotate(M_PI * 8);
+		expect(approx(a.x, sqrt(2) / 2., 1e-6));
+		expect(approx(a.y, sqrt(2) / 2., 1e-6));
 	};
 };
 } // namespace
