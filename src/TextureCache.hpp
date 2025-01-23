40746d7a1209967a3c017be6826606079fec4062
Jan Niklas Hasse <jhasse@bixense.com>
2025-01-23 18:35:59 +0100

    #include <unordered_map> in TextureCache.hpp

--- src/TextureCache.hpp
+++ src/TextureCache.hpp
@@ -1,10 +1,11 @@
-// Copyright 2024 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2024-2025 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 #pragma once
 
 #include "jngl/Singleton.hpp"
 
 #include <memory>
+#include <unordered_map>
 
 namespace jngl {
 
