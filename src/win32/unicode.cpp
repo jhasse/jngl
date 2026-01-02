e844d791277ac2a4ceb2d39a0e24fe23f9c94b09
Jan Niklas Hasse <jhasse@bixense.com>
2026-01-02 12:52:19 +0100

    Windows: Replace deprecated std::codecvt with Windows API for UTF conversion

--- src/win32/unicode.cpp
+++ src/win32/unicode.cpp
@@ -1,37 +1,56 @@
-// Copyright 2020-2024 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2020-2026 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "unicode.hpp"
 
-#include <codecvt>
-#include <locale>
+#include <stdexcept>
+#include <windows.h>
 
 namespace jngl {
 
-#ifndef _MSC_VER
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
 std::wstring utf8ToUtf16(std::string_view s) {
-	std::wstring wstr =
-	    std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(std::string(s).c_str());
-#if defined(__MINGW32__) && __GNUC__ < 8
-	// Workaround for https://sourceforge.net/p/mingw-w64/bugs/538/
-	for (wchar_t& wch : wstr) {
-		wch = int(wch) / 256;
+	if (s.empty()) {
+		return {};
 	}
-#endif
-	return wstr;
+
+	const int requiredSize = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, s.data(),
+	                                             static_cast<int>(s.size()), nullptr, 0);
+	if (requiredSize == 0) {
+		throw std::runtime_error("utf8ToUtf16: MultiByteToWideChar size query failed");
+	}
+
+	std::wstring result(static_cast<size_t>(requiredSize), L'\0');
+	const int written =
+	    MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, s.data(), static_cast<int>(s.size()),
+	                        result.data(), requiredSize);
+	if (written == 0) {
+		throw std::runtime_error("utf8ToUtf16: MultiByteToWideChar conversion failed");
+	}
+
+	return result;
 }
 
 std::string utf16ToUtf8(std::wstring_view wstr) {
-	return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>>().to_bytes(
-	    std::wstring(wstr).c_str());
-}
+	if (wstr.empty()) {
+		return {};
+	}
 
-#ifndef _MSC_VER
-#pragma GCC diagnostic pop
-#endif
+	const int requiredSize =
+	    WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, wstr.data(),
+	                        static_cast<int>(wstr.size()), nullptr, 0, nullptr, nullptr);
+	if (requiredSize == 0) {
+		throw std::runtime_error("utf16ToUtf8: WideCharToMultiByte size query failed");
+	}
+
+	std::string result(static_cast<size_t>(requiredSize), '\0');
+	const int written = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, wstr.data(),
+	                                        static_cast<int>(wstr.size()), result.data(),
+	                                        requiredSize, nullptr, nullptr);
+	if (written == 0) {
+		throw std::runtime_error("utf16ToUtf8: WideCharToMultiByte conversion failed");
+	}
+
+	return result;
+}
 
 } // namespace jngl
