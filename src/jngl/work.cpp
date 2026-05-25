9511ea451fd13ef3dd3358f9aa3864c146dfcf62
Jan Niklas Hasse <jhasse@bixense.com>
2026-05-25 14:58:28 +0200

    Also call onQuitEvent() on platforms that can't quit (i.e. Web)

--- src/jngl/work.cpp
+++ src/jngl/work.cpp
@@ -1,4 +1,4 @@
-// Copyright 2012-2024 Jan Niklas Hasse <jhasse@bixense.com>
+// Copyright 2012-2026 Jan Niklas Hasse <jhasse@bixense.com>
 // For conditions of distribution and use, see copyright notice in LICENSE.txt
 
 #include "work.hpp"
@@ -8,7 +8,14 @@
 namespace jngl {
 
 void Work::onBackEvent() {
-	jngl::quit();
+	if (canQuit()) {
+		quit();
+	} else {
+		/// On platforms like the web quit() won't do anything. If the game uses onQuitEvent() to
+		/// show a pause menu, call it manually here so that pressing Escape (which calls
+		/// onBackEvent()) will open the pause menu instead of doing nothing.
+		onQuitEvent();
+	}
 }
 
 void Work::onQuitEvent() {
